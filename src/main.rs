3af68e7d8d3ec7613d9ace18881991f0dc853087
drkrssll <russellderek01@gmail.com>
2025-03-18 18:33:14 -0400

    Working on a LevelUp implementation

--- src/main.rs
+++ src/main.rs
@@ -3,8 +3,6 @@ extern crate rocket;
 #[macro_use]
 extern crate diesel;
 
-use std::collections::HashMap;
-
 use diesel::prelude::*;
 use rocket::serde::json::Json;
 use rocket::State;
@@ -16,6 +14,12 @@ mod schema;
 use db::DbPool;
 use models::{ErrorResponse, NewPlayer, Player, UpdatePlayer};
 use schema::players;
+use serde::Deserialize;
+
+#[derive(Deserialize)]
+struct RankUpdate {
+    rank: String,
+}
 
 #[get("/players/<player_id>/bank")]
 fn get_player_bank(pool: &State<DbPool>, player_id: i32) -> Option<String> {
@@ -63,20 +67,20 @@ fn get_player(pool: &State<DbPool>, player_id: i32) -> Result<Json<Player>, Json
 fn level_up_player(
     pool: &State<DbPool>,
     player_id: i32,
-    new_rank: Json<String>,
+    new_rank: Json<RankUpdate>,
 ) -> Option<Json<Player>> {
     let ranks = vec![
         "A0", "A1", "A2", "A3", "A4", "A5", "A6", "A7", "A8", "A9", "B0", "B1", "B2", "B3", "B4",
         "B5", "B6", "B7", "B8", "B9", "C0", "C1", "C2", "C3", "C4", "C5", "C6", "C7", "C8", "C9",
     ];
 
-    if !ranks.contains(&new_rank.as_str()) {
+    if !ranks.contains(&new_rank.rank.as_str()) {
         return None;
     }
 
     let connection = &mut pool.get().expect("Failed to get connection");
     let updated_rank = diesel::update(players::table.find(player_id))
-        .set(players::rank.eq(&*new_rank))
+        .set(players::rank.eq(&*new_rank.rank))
         .get_result::<Player>(connection)
         .optional()
         .expect("Error leveling up player");
